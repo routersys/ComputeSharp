0ba0ac7144966a88942f7bb3de2067d82d5ab26e
routersys <github.routersys@gmail.com>
2026-07-24 22:08:11 +0900

    パイプラインSchemaバージョン定数を追加

---
+++ src/ComputeSharp/Graphics/Pipelines/PipelineSchema.cs
@@ -0,0 +1,10 @@
+namespace ComputeSharp.Graphics.Pipelines;
+
+internal static class PipelineSchema
+{
+    public const ushort Major = 1;
+
+    public const ushort Minor = 0;
+
+    public const ushort Descriptor = 1;
+}
