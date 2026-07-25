a23f429fbcbffd07631786444dd3596dbf818b3e
routersys <github.routersys@gmail.com>
2026-07-25 09:46:36 +0900

    Generation所有者を正本とするHandleとPinを追加

--- src/ComputeSharp/Graphics/Resources/Lifetime/ResourceGenerationOwner.cs
+++ src/ComputeSharp/Graphics/Resources/Lifetime/ResourceGenerationOwner.cs
@@ -1,3 +1,4 @@
+using System;
 using ComputeSharp.Graphics.Pipelines;
 
 namespace ComputeSharp.Resources.Lifetime;
@@ -11,13 +12,35 @@ internal interface IResourceGenerationOwner
     ref ResourceGenerationRecord GetResourceRecord(int resourceOrdinal);
 }
 
-internal readonly struct ResourceGenerationSetHandle(ResourceGenerationSetId setId, IResourceGenerationOwner owner)
+internal readonly struct ResourceGenerationSetHandle
 {
-    public ResourceGenerationSetId SetId { get; } = setId;
+    public ResourceGenerationSetHandle(IResourceGenerationOwner owner)
+    {
+        default(ArgumentNullException).ThrowIfNull(owner);
 
-    public IResourceGenerationOwner Owner { get; } = owner;
+        if (owner.SetId.Value == 0 || owner.ResourceCount <= 0)
+        {
+            default(ArgumentException).Throw(nameof(owner), "The generation owner is invalid.");
+        }
+
+        Owner = owner;
+        SetId = owner.SetId;
+    }
+
+    public ResourceGenerationSetId SetId { get; }
+
+    public IResourceGenerationOwner Owner { get; }
 
     public bool IsEmpty => Owner is null;
+}
+
+internal readonly struct ResourceGenerationPin(ResourceGenerationSetHandle handle, ResourceGenerationId generationId, int resourceIndex)
+{
+    public ResourceGenerationSetHandle Handle { get; } = handle;
+
+    public ResourceGenerationId GenerationId { get; } = generationId;
+
+    public int ResourceIndex { get; } = resourceIndex;
 }
 
 internal readonly struct ResourceBindingRecord(
