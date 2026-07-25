f3ab459352f2c9efed4453156e3818ae04fd4cb2
routersys <github.routersys@gmail.com>
2026-07-25 14:16:45 +0900

    Host契約モデルへ上限検証を接続

--- src/ComputeSharp.SourceGenerators/Helpers/PipelineHostContractModelBuilder.cs
+++ src/ComputeSharp.SourceGenerators/Helpers/PipelineHostContractModelBuilder.cs
@@ -64,7 +64,7 @@ internal static class PipelineHostContractModelBuilder
             orderedPipelines,
             slots);
 
-        return true;
+        return PipelineContractLimitValidator.IsWithinLimits(host);
     }
 
     /// <summary>
