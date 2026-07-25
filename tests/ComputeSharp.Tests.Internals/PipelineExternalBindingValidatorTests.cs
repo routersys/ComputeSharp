87d2db006a00994029549516664f45a77a118b35
routersys <github.routersys@gmail.com>
2026-07-25 13:07:50 +0900

    外部Binding検証のテストを追加

---
+++ tests/ComputeSharp.Tests.Internals/PipelineExternalBindingValidatorTests.cs
@@ -0,0 +1,106 @@
+using System.IO;
+using ComputeSharp.Graphics.Pipelines;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ComputeSharp.Tests.Internals;
+
+[TestClass]
+public class PipelineExternalBindingValidatorTests
+{
+    private const string TextureTypeMetadataName = "ComputeSharp.ReadWriteTexture2D`2[ComputeSharp.Bgra32,ComputeSharp.Float4]";
+
+    private static ResourceContractDescriptor Parameter(
+        string resourceTypeMetadataName = TextureTypeMetadataName,
+        ComputeResourceAccess access = ComputeResourceAccess.ReadWrite,
+        ComputeResourceSharing sharing = ComputeResourceSharing.External,
+        ResourceOwnershipKind ownership = ResourceOwnershipKind.Borrowed,
+        bool hasSlot = false,
+        uint slot = 0,
+        uint slotResourceIndex = 0)
+    {
+        return new ResourceContractDescriptor(
+            new ResourceOrdinal(0),
+            resourceTypeMetadataName,
+            access,
+            sharing,
+            ComputeResourceAliasing.Disallow,
+            ownership,
+            hasSlot,
+            new SlotOrdinal(slot),
+            slotResourceIndex);
+    }
+
+    private static SharedTextureContractDescriptor SharedTexture(
+        string resourceTypeMetadataName = TextureTypeMetadataName,
+        ComputeResourceAccess computeAccess = ComputeResourceAccess.ReadWrite)
+    {
+        return new SharedTextureContractDescriptor(
+            new SlotOrdinal(0),
+            "Source",
+            resourceTypeMetadataName,
+            ComputeResourceResizePolicy.Exact,
+            computeAccess,
+            ExternalResourceAccess.Write,
+            ExternalTextureUsage.RenderTarget,
+            ComputeAlphaMode.Premultiplied,
+            ComputeSharedTextureInitialOwner.External,
+            ComputeResourceRecovery.RecreateFromHost);
+    }
+
+    [TestMethod]
+    public void AcceptsMatchingExternalParameter()
+    {
+        PipelineExternalBindingValidator.Validate(Parameter(), SharedTexture());
+    }
+
+    [TestMethod]
+    public void RejectsInternalSharing()
+    {
+        _ = Assert.ThrowsException<InvalidDataException>(
+            () => PipelineExternalBindingValidator.Validate(Parameter(sharing: ComputeResourceSharing.Internal), SharedTexture()));
+    }
+
+    [TestMethod]
+    public void RejectsOwnershipOtherThanBorrowed()
+    {
+        _ = Assert.ThrowsException<InvalidDataException>(
+            () => PipelineExternalBindingValidator.Validate(Parameter(ownership: ResourceOwnershipKind.SharedTextureSlot), SharedTexture()));
+
+        _ = Assert.ThrowsException<InvalidDataException>(
+            () => PipelineExternalBindingValidator.Validate(Parameter(ownership: ResourceOwnershipKind.OwnedSlot), SharedTexture()));
+    }
+
+    [TestMethod]
+    public void RejectsParameterBoundToSlot()
+    {
+        _ = Assert.ThrowsException<InvalidDataException>(
+            () => PipelineExternalBindingValidator.Validate(Parameter(hasSlot: true), SharedTexture()));
+
+        _ = Assert.ThrowsException<InvalidDataException>(
+            () => PipelineExternalBindingValidator.Validate(Parameter(slot: 1), SharedTexture()));
+
+        _ = Assert.ThrowsException<InvalidDataException>(
+            () => PipelineExternalBindingValidator.Validate(Parameter(slotResourceIndex: 1), SharedTexture()));
+    }
+
+    [TestMethod]
+    public void RejectsMismatchedResourceType()
+    {
+        _ = Assert.ThrowsException<InvalidDataException>(
+            () => PipelineExternalBindingValidator.Validate(Parameter(resourceTypeMetadataName: "ComputeSharp.ReadWriteBuffer`1[System.Int32]"), SharedTexture()));
+    }
+
+    [TestMethod]
+    public void RejectsMismatchedComputeAccess()
+    {
+        _ = Assert.ThrowsException<InvalidDataException>(
+            () => PipelineExternalBindingValidator.Validate(Parameter(access: ComputeResourceAccess.Read), SharedTexture()));
+    }
+
+    [TestMethod]
+    public void AcceptsSameResourceTypeForDistinctSharedTextures()
+    {
+        PipelineExternalBindingValidator.Validate(Parameter(), SharedTexture());
+        PipelineExternalBindingValidator.Validate(Parameter(), SharedTexture());
+    }
+}
