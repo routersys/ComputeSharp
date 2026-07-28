76c769220d72e62683ccbd718d79233019b95a73
routersys <github.routersys@gmail.com>
2026-07-29 00:09:01 +0900

    Texture3Dの現在D3D12状態をRecordへ一元化する

--- src/ComputeSharp/Graphics/Resources/Abstract/Texture3D{T}.cs
+++ src/ComputeSharp/Graphics/Resources/Abstract/Texture3D{T}.cs
@@ -71,10 +71,7 @@ public abstract unsafe partial class Texture3D<T> : IReferenceTrackedObject, IGr
     /// </summary>
     private readonly D3D12_PLACED_SUBRESOURCE_FOOTPRINT d3D12PlacedSubresourceFootprint;
 
-    /// <summary>
-    /// The current <see cref="D3D12_RESOURCE_STATES"/> value for the current resource.
-    /// </summary>
-    private D3D12_RESOURCE_STATES d3D12ResourceState;
+    private readonly TrackedResourceState residentState;
 
     private D3D12ComputeFenceTracker d3D12ComputeFenceTracker;
 
@@ -116,18 +113,20 @@ public abstract unsafe partial class Texture3D<T> : IReferenceTrackedObject, IGr
             (ushort)depth,
             out this.allocation,
             out this.d3D12Resource,
-            out this.d3D12ResourceState);
+            out D3D12_RESOURCE_STATES d3D12ResourceState);
+
+        this.residentState = ComputeGenerationDescriber.GetTrackedState(d3D12ResourceState);
 
         this.generationBinding.InitializeObservedAccess(ComputeGenerationDescriber.GetObservedAccess(resourceType));
 
         this.generationBinding.InitializeSelfOwned(
             this,
             device.ResourceIdentities,
-            ComputeGenerationDescriber.GetTrackedState(this.d3D12ResourceState),
+            this.residentState,
             this.memoryAllocation.Placement,
             this.memoryAllocation.Bytes);
 
-        this.d3D12CommandListType = this.d3D12ResourceState == D3D12_RESOURCE_STATE_COMMON
+        this.d3D12CommandListType = this.residentState is TrackedResourceState.Common
             ? D3D12_COMMAND_LIST_TYPE_COPY
             : D3D12_COMMAND_LIST_TYPE_COMPUTE;
 
@@ -205,9 +204,7 @@ public abstract unsafe partial class Texture3D<T> : IReferenceTrackedObject, IGr
     /// <inheritdoc/>
     bool IGenerationBoundResource.TryGetGenerationBinding(out ResourceUsageBinding binding)
     {
-        return this.generationBinding.TryGetBinding(
-            ComputeGenerationDescriber.GetTrackedState(this.d3D12ResourceState),
-            out binding);
+        return this.generationBinding.TryGetBinding(this.residentState, out binding);
     }
 
     /// <summary>
@@ -226,6 +223,14 @@ public abstract unsafe partial class Texture3D<T> : IReferenceTrackedObject, IGr
     void ID3D12ComputeFenceTrackedResource.MarkComputeFence(ulong d3D12FenceValue)
     {
         this.d3D12ComputeFenceTracker.Mark(d3D12FenceValue);
+    }
+
+    private D3D12_RESOURCE_STATES GetD3D12ResourceState()
+    {
+        lock (GraphicsDevice.HazardGate)
+        {
+            return ComputeGenerationDescriber.GetD3D12ResourceStates(this.generationBinding.ActiveRecord.D3D12State);
+        }
     }
 
     /// <summary>
@@ -276,13 +281,15 @@ public abstract unsafe partial class Texture3D<T> : IReferenceTrackedObject, IGr
             out *&allocation,
             out *&d3D12Resource);
 
+        D3D12_RESOURCE_STATES d3D12ResourceState = GetD3D12ResourceState();
+
         using (CommandList copyCommandList = new(GraphicsDevice, this.d3D12CommandListType))
         {
             copyCommandList.AddComputeFenceWait(D3D12ComputeFenceValue);
 
             if (copyCommandList.D3D12CommandListType == D3D12_COMMAND_LIST_TYPE_COMPUTE)
             {
-                copyCommandList.D3D12GraphicsCommandList->TransitionBarrier(D3D12Resource, this.d3D12ResourceState, D3D12_RESOURCE_STATE_COPY_SOURCE);
+                copyCommandList.D3D12GraphicsCommandList->TransitionBarrier(D3D12Resource, d3D12ResourceState, D3D12_RESOURCE_STATE_COPY_SOURCE);
             }
 
             copyCommandList.D3D12GraphicsCommandList->CopyTextureRegion(
@@ -301,7 +308,7 @@ public abstract unsafe partial class Texture3D<T> : IReferenceTrackedObject, IGr
 
             if (copyCommandList.D3D12CommandListType == D3D12_COMMAND_LIST_TYPE_COMPUTE)
             {
-                copyCommandList.D3D12GraphicsCommandList->TransitionBarrier(D3D12Resource, D3D12_RESOURCE_STATE_COPY_SOURCE, this.d3D12ResourceState);
+                copyCommandList.D3D12GraphicsCommandList->TransitionBarrier(D3D12Resource, D3D12_RESOURCE_STATE_COPY_SOURCE, d3D12ResourceState);
             }
 
             copyCommandList.ExecuteAndWaitForCompletion();
@@ -370,6 +377,9 @@ public abstract unsafe partial class Texture3D<T> : IReferenceTrackedObject, IGr
             ? D3D12_COMMAND_LIST_TYPE_COMPUTE
             : D3D12_COMMAND_LIST_TYPE_COPY;
 
+        D3D12_RESOURCE_STATES d3D12ResourceState = GetD3D12ResourceState();
+        D3D12_RESOURCE_STATES d3D12DestinationResourceState = destination.GetD3D12ResourceState();
+
         using CommandList copyCommandList = new(GraphicsDevice, d3D12CommandListType);
 
         copyCommandList.AddComputeFenceWait(D3D12ComputeFenceValue);
@@ -377,8 +387,8 @@ public abstract unsafe partial class Texture3D<T> : IReferenceTrackedObject, IGr
 
         if (copyCommandList.D3D12CommandListType == D3D12_COMMAND_LIST_TYPE_COMPUTE)
         {
-            copyCommandList.D3D12GraphicsCommandList->TransitionBarrier(D3D12Resource, this.d3D12ResourceState, D3D12_RESOURCE_STATE_COPY_SOURCE);
-            copyCommandList.D3D12GraphicsCommandList->TransitionBarrier(destination.D3D12Resource, destination.d3D12ResourceState, D3D12_RESOURCE_STATE_COPY_DEST);
+            copyCommandList.D3D12GraphicsCommandList->TransitionBarrier(D3D12Resource, d3D12ResourceState, D3D12_RESOURCE_STATE_COPY_SOURCE);
+            copyCommandList.D3D12GraphicsCommandList->TransitionBarrier(destination.D3D12Resource, d3D12DestinationResourceState, D3D12_RESOURCE_STATE_COPY_DEST);
         }
 
         copyCommandList.D3D12GraphicsCommandList->CopyTextureRegion(
@@ -396,8 +406,8 @@ public abstract unsafe partial class Texture3D<T> : IReferenceTrackedObject, IGr
 
         if (copyCommandList.D3D12CommandListType == D3D12_COMMAND_LIST_TYPE_COMPUTE)
         {
-            copyCommandList.D3D12GraphicsCommandList->TransitionBarrier(D3D12Resource, D3D12_RESOURCE_STATE_COPY_SOURCE, this.d3D12ResourceState);
-            copyCommandList.D3D12GraphicsCommandList->TransitionBarrier(destination.D3D12Resource, D3D12_RESOURCE_STATE_COPY_DEST, destination.d3D12ResourceState);
+            copyCommandList.D3D12GraphicsCommandList->TransitionBarrier(D3D12Resource, D3D12_RESOURCE_STATE_COPY_SOURCE, d3D12ResourceState);
+            copyCommandList.D3D12GraphicsCommandList->TransitionBarrier(destination.D3D12Resource, D3D12_RESOURCE_STATE_COPY_DEST, d3D12DestinationResourceState);
         }
 
         copyCommandList.ExecuteAndWaitForCompletion();
@@ -445,13 +455,15 @@ public abstract unsafe partial class Texture3D<T> : IReferenceTrackedObject, IGr
 
         destination.ThrowIfDeviceMismatch(GraphicsDevice);
 
+        D3D12_RESOURCE_STATES d3D12ResourceState = GetD3D12ResourceState();
+
         using CommandList copyCommandList = new(GraphicsDevice, this.d3D12CommandListType);
 
         copyCommandList.AddComputeFenceWait(D3D12ComputeFenceValue);
 
         if (copyCommandList.D3D12CommandListType == D3D12_COMMAND_LIST_TYPE_COMPUTE)
         {
-            copyCommandList.D3D12GraphicsCommandList->TransitionBarrier(D3D12Resource, this.d3D12ResourceState, D3D12_RESOURCE_STATE_COPY_SOURCE);
+            copyCommandList.D3D12GraphicsCommandList->TransitionBarrier(D3D12Resource, d3D12ResourceState, D3D12_RESOURCE_STATE_COPY_SOURCE);
         }
 
         fixed (D3D12_PLACED_SUBRESOURCE_FOOTPRINT* d3D12PlacedSubresourceFootprintDestination = &destination.D3D12PlacedSubresourceFootprint)
@@ -473,7 +485,7 @@ public abstract unsafe partial class Texture3D<T> : IReferenceTrackedObject, IGr
 
         if (copyCommandList.D3D12CommandListType == D3D12_COMMAND_LIST_TYPE_COMPUTE)
         {
-            copyCommandList.D3D12GraphicsCommandList->TransitionBarrier(D3D12Resource, D3D12_RESOURCE_STATE_COPY_SOURCE, this.d3D12ResourceState);
+            copyCommandList.D3D12GraphicsCommandList->TransitionBarrier(D3D12Resource, D3D12_RESOURCE_STATE_COPY_SOURCE, d3D12ResourceState);
         }
 
         copyCommandList.ExecuteAndWaitForCompletion();
@@ -542,13 +554,15 @@ public abstract unsafe partial class Texture3D<T> : IReferenceTrackedObject, IGr
             }
         }
 
+        D3D12_RESOURCE_STATES d3D12ResourceState = GetD3D12ResourceState();
+
         using CommandList copyCommandList = new(GraphicsDevice, this.d3D12CommandListType);
 
         copyCommandList.AddComputeFenceWait(D3D12ComputeFenceValue);
 
         if (copyCommandList.D3D12CommandListType == D3D12_COMMAND_LIST_TYPE_COMPUTE)
         {
-            copyCommandList.D3D12GraphicsCommandList->TransitionBarrier(D3D12Resource, this.d3D12ResourceState, D3D12_RESOURCE_STATE_COPY_DEST);
+            copyCommandList.D3D12GraphicsCommandList->TransitionBarrier(D3D12Resource, d3D12ResourceState, D3D12_RESOURCE_STATE_COPY_DEST);
         }
 
         copyCommandList.D3D12GraphicsCommandList->CopyTextureRegion(
@@ -567,7 +581,7 @@ public abstract unsafe partial class Texture3D<T> : IReferenceTrackedObject, IGr
 
         if (copyCommandList.D3D12CommandListType == D3D12_COMMAND_LIST_TYPE_COMPUTE)
         {
-            copyCommandList.D3D12GraphicsCommandList->TransitionBarrier(D3D12Resource, D3D12_RESOURCE_STATE_COPY_DEST, this.d3D12ResourceState);
+            copyCommandList.D3D12GraphicsCommandList->TransitionBarrier(D3D12Resource, D3D12_RESOURCE_STATE_COPY_DEST, d3D12ResourceState);
         }
 
         copyCommandList.ExecuteAndWaitForCompletion();
@@ -615,13 +629,15 @@ public abstract unsafe partial class Texture3D<T> : IReferenceTrackedObject, IGr
 
         source.ThrowIfDeviceMismatch(GraphicsDevice);
 
+        D3D12_RESOURCE_STATES d3D12ResourceState = GetD3D12ResourceState();
+
         using CommandList copyCommandList = new(GraphicsDevice, this.d3D12CommandListType);
 
         copyCommandList.AddComputeFenceWait(D3D12ComputeFenceValue);
 
         if (copyCommandList.D3D12CommandListType == D3D12_COMMAND_LIST_TYPE_COMPUTE)
         {
-            copyCommandList.D3D12GraphicsCommandList->TransitionBarrier(D3D12Resource, this.d3D12ResourceState, D3D12_RESOURCE_STATE_COPY_DEST);
+            copyCommandList.D3D12GraphicsCommandList->TransitionBarrier(D3D12Resource, d3D12ResourceState, D3D12_RESOURCE_STATE_COPY_DEST);
         }
 
         fixed (D3D12_PLACED_SUBRESOURCE_FOOTPRINT* d3D12PlacedSubresourceFootprintSource = &source.D3D12PlacedSubresourceFootprint)
@@ -643,7 +659,7 @@ public abstract unsafe partial class Texture3D<T> : IReferenceTrackedObject, IGr
 
         if (copyCommandList.D3D12CommandListType == D3D12_COMMAND_LIST_TYPE_COMPUTE)
         {
-            copyCommandList.D3D12GraphicsCommandList->TransitionBarrier(D3D12Resource, D3D12_RESOURCE_STATE_COPY_DEST, this.d3D12ResourceState);
+            copyCommandList.D3D12GraphicsCommandList->TransitionBarrier(D3D12Resource, D3D12_RESOURCE_STATE_COPY_DEST, d3D12ResourceState);
         }
 
         copyCommandList.ExecuteAndWaitForCompletion();
@@ -668,7 +684,7 @@ public abstract unsafe partial class Texture3D<T> : IReferenceTrackedObject, IGr
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     protected void ThrowIfIsNotInReadOnlyState()
     {
-        if (this.d3D12ResourceState == D3D12_RESOURCE_STATE_UNORDERED_ACCESS)
+        if (GetD3D12ResourceState() == D3D12_RESOURCE_STATE_UNORDERED_ACCESS)
         {
             static void Throw()
             {
@@ -722,14 +738,15 @@ public abstract unsafe partial class Texture3D<T> : IReferenceTrackedObject, IGr
 
         ThrowIfDeviceMismatch(device);
 
-        D3D12_RESOURCE_STATES d3D12ResourceStatesBefore = this.d3D12ResourceState;
         D3D12_RESOURCE_STATES d3D12ResourceStatesAfter = ResourceStateHelper.GetD3D12ResourceStates(resourceState);
-
-        this.d3D12ResourceState = d3D12ResourceStatesAfter;
+        D3D12_RESOURCE_STATES d3D12ResourceStatesBefore;
 
         lock (device.HazardGate)
         {
-            this.generationBinding.Record.D3D12State = ComputeGenerationDescriber.GetTrackedState(d3D12ResourceStatesAfter);
+            ref ResourceGenerationRecord record = ref this.generationBinding.ActiveRecord;
+
+            d3D12ResourceStatesBefore = ComputeGenerationDescriber.GetD3D12ResourceStates(record.D3D12State);
+            record.D3D12State = ComputeGenerationDescriber.GetTrackedState(d3D12ResourceStatesAfter);
         }
 
         d3D12Resource = D3D12Resource;
