f96cf6db7611f287882e5f2d7a6d948865f9aa7a
routersys <github.routersys@gmail.com>
2026-08-25 17:11:45 +0900

    D2D1の試験2本を戻してsolutionへ登録する

---
+++ tests/ComputeWeave.D2D1.Tests.AssemblyLevelAttributes/Properties/AssemblyInfo.cs
@@ -0,0 +1,4 @@
+using ComputeWeave.D2D1;
+
+[assembly: D2DShaderProfile(D2D1ShaderProfile.PixelShader41)]
+[assembly: D2DCompileOptions(D2D1CompileOptions.IeeeStrictness | D2D1CompileOptions.OptimizationLevel2 | D2D1CompileOptions.PartialPrecision)]
